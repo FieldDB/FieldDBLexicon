7ae3612eac33c4d424cd0887da38252c3a750238
cesine <cesine@yahoo.com>
2015-06-21 16:40:12 -0400

    removing malarkey and toastr

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -2,15 +2,10 @@
   'use strict';
 
   /** @ngInject */
-  function config($logProvider, toastr) {
+  function config($logProvider) {
     // Enable log
     $logProvider.debugEnabled(true);
 
-    // Set options third-party lib
-    toastr.options.timeOut = 3000;
-    toastr.options.positionClass = 'toast-top-right';
-    toastr.options.preventDuplicates = true;
-    toastr.options.progressBar = true;
   }
 
   angular
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,36 +6,13 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
+  function MainController($scope) {
+   
+    $scope.corpus = {
+      pouchname: 'glossersample-quechua'
+    };
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1434659337554;
-    vm.showToastr = showToastr;
+    // iLanguageCloud({orthography: 'A cloud is a visible mass ...'}).render();
 
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    iLanguageCloud({orthography: 'A cloud is a visible mass ...'}).render();
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
