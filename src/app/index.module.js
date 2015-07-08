ff16eda8c0418808cf99a29b1509a63f1b46cb67
louisa-bielig <louisa.bielig@gmail.com>
2015-07-08 20:09:21 +0200

    added fielddb-angular

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,13 +1,17 @@
 (function() {
 	'use strict';
+	FieldDB.Database.prototype.BASE_DB_URL = 'http://localhost:5984';
+	FieldDB.Database.prototype.BASE_AUTH_URL = 'https://authdev.lingsync.org';
 
 	angular.module('fielddbLexiconAngularApp', [
 		'ngAnimate',
 		'ngCookies',
 		'ngTouch',
 		'ngSanitize',
-		'ngRoute',
+		'ui.router',
 		'mgcrea.ngStrap',
-		'WordCloudApp'	]);
+		'WordCloudApp',
+		'fielddbAngular'
+	]);
 
 })();
