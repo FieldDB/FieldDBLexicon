eba1c48697097c1d5a9199839243da9fd2f5c00a
cesine <cesine@yahoo.com>
2015-08-06 00:22:46 -0400

    added lexical entry route

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,22 +1,49 @@
 (function() {
   'use strict';
+  /* globals FieldDB */
+  
+  var routeConfig = function($stateProvider, $urlRouterProvider) {
 
-  function routeConfig($stateProvider, $urlRouterProvider) {
+    var passStateParamsController = function($stateParams) {
+      console.log('Loading ', $stateParams);
+      var paramsChanged = false;
+      if (FieldDB && FieldDB.FieldDBObject && FieldDB.FieldDBObject.application) {
+        var fieldDBApp = FieldDB.FieldDBObject.application;
+        if (!fieldDBApp.routeParams) {
+          paramsChanged = true;
+        } else {
+          for (var param in $stateParams) {
+            if ($stateParams.hasOwnProperty(param) && fieldDBApp.routeParams[param] !== $stateParams[param]) {
+              paramsChanged = true;
+            }
+          }
+        }
+        if (paramsChanged) {
+          fieldDBApp.processRouteParams($stateParams);
+          fieldDBApp.debug(fieldDBApp.routeParams);
+        }
+      }
+    };
 
     $stateProvider
-      .state("lexicon", {
-        url: "^/:team/:corpusidentifier/lexicon",
+      .state('lexicon.lexicalEntry', {
+        url: '^/:team/:corpusidentifier/lexicon/:lexicalentry',
         templateUrl: 'app/main/lexicon-browser.html',
-        controller: 'MainController',
-        controllerAs: 'main'
+        controller: passStateParamsController
       });
     $stateProvider
-      .state("faq", {
-        url: "^/faq",
+      .state('lexicon', {
+        url: '^/:team/:corpusidentifier/lexicon',
+        templateUrl: 'app/main/lexicon-browser.html',
+        controller: passStateParamsController
+      });
+    $stateProvider
+      .state('faq', {
+        url: '^/faq',
         templateUrl: 'app/components/faq/faq.html'
       });
     $urlRouterProvider.otherwise('/chuj/shared/lexicon');
-  }
+  };
 
   angular
     .module('fielddbLexiconAngularApp')
