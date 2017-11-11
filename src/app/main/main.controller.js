4e541b812f8912e2ea25e55ff02884f2fe9be9a7
cesine <cesine@yahoo.com>
2017-11-11 11:38:36 -0500

    updated dependencies

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,12 +7,12 @@
 
   /** @ngInject */
   function MainController($scope) {
-   
+
     $scope.corpus = {
       pouchname: 'glossersample-quechua'
     };
 
-    // iLanguageCloud({orthography: 'A cloud is a visible mass ...'}).render();
+    // new ILanguageCloud({orthography: 'A cloud is a visible mass ...'}).render();
 
   }
 })();
